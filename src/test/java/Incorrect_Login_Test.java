f3e56217eace65be295a722eb178161bd4d5a3a5
Krzysztof Juszczyk <100209790+krzysztofjuszczyk@users.noreply.github.com>
2023-09-28 11:52:44 +0200

    correct & incorrect login tests

--- src/test/java/Incorrect_Login_Test.java
+++ src/test/java/Incorrect_Login_Test.java
@@ -1,17 +1,17 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.openqa.selenium.WebDriver;
 
 import java.time.Duration;
 
-public class Correct_Login_Test {
+public class Incorrect_Login_Test {
     @Test
     public void correctLoginTest(){
-        String name = "standard_user";
-        String password = "secret_sauce";
+            String name = "standard_user";
+        String incorrectPassword1 = "secret_sauce1";
         String webpage = "http://saucedemo.com";
 
         System.setProperty("webdriver.chrome.driver", "c:/dev/driver/chromedriver.exe");
@@ -25,10 +25,10 @@ public class Correct_Login_Test {
         WebElement loginBtn = driver.findElement(By.cssSelector("#login-button"));
 
         usernameTxt.sendKeys(name);
-        passwordTxt.sendKeys(password);
+        passwordTxt.sendKeys(incorrectPassword1);
         loginBtn.click();
 
-        WebElement productsTxt = driver.findElement(By.className("title"));
-        Assert.assertEquals(productsTxt.getText(), "Products");
+        WebElement errorElm = driver.findElement(By.cssSelector(".error-message-container > h3:nth-child(1)"));
+        Assert.assertEquals(errorElm.getText(),"Epic sadface: Username and password do not match any user in this service");
     }
 }
